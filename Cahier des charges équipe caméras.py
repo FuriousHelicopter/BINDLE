597b979520c5f5eaa307cc4b9bfc7709362be24a
Timothée VAUCHY <timvauchy@gmail.com>
2022-10-19 21:52:22 +0200

    CdC feature camera

---
+++ Cahier des charges équipe caméras.py
@@ -0,0 +1,18 @@
+"""
+
+Objectif :
+    - Exécuter en temps réel la fonction traitement_image_cam_ligne (pas à remplir pour l'instant) sur les images de la caméra ligne
+    - Enregistrer la caméra principale (cf ffmpeg) en temps réel dans un fichier (/!\ Utiliser l'accélération matérielle)
+
+Conditions validation :
+    - Programmation Orientée Objet (encapsuler toutes vos fonctions dans des classes bien propres => aide au besoin)
+    - Framerate stable
+    - Utilisation CPU résonable
+
+"""
+
+def traitement_image_cam_ligne(image):
+    pass
+
+if __name__ == '__main__':
+    pass
